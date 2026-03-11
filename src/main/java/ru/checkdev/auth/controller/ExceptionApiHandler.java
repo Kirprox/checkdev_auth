14812e943457c4ead9b9704028d693156f127ef0
kir <kirpro95@yandex.ru>
2026-03-11 12:34:46 +0300

    Обработка исключений в RestTemplate [#505058]

---
+++ src/main/java/ru/checkdev/auth/controller/ExceptionApiHandler.java
@@ -0,0 +1,19 @@
+package ru.checkdev.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.checkdev.auth.exception.IdNotFoundException;
+import ru.checkdev.auth.model.ErrorMessage;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<ErrorMessage> notFoundException(IdNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+}
