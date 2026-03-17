c358c7cafe2fea3aa46f8589dbc298a7a69ad9f7
kir <kirpro95@yandex.ru>
2026-03-17 11:43:38 +0300

    add exception handler

---
+++ src/main/java/ru/checkdev/auth/exception/IdNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.checkdev.auth.exception;
+
+public class IdNotFoundException extends AppException {
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/checkdev/auth/model/ErrorMessage.java
@@ -0,0 +1,13 @@
+package ru.checkdev.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private String message;
+}
